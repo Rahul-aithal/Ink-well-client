2df9e87ac113eb3ffcb6a6f38bc925c1a0db4954
Rahul-aithal <aithalrahul34@gmail.com>
2024-08-04 16:26:35 +0530

    AuthSlice  setup

---
+++ src/store/AuthSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState={
+    status:false,
+    userData:null,  
+}
+
+export const authSlice =createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            state.status=true;
+            state.userData=action.payload.userData;
+            sessionStorage.setItem("authState",JSON.stringify(state));
+        },
+        logout:(state)=>{
+            state.status=false;
+            state.userData=null;
+            sessionStorage.removeItem("authState");
+        },
+        initializeAuthFromSesionStorgae:(state)=>{
+            const storedState = sessionStorage.getItem("authState");
+            if(storedState){
+                const parsedSate= JSON.parse(storedState);
+                state.status=parsedSate.status;
+                state.userData=parsedSate.userData;
+            }
+        }
+    }
+}) ;
+
+
+export const { login, logout,initializeAuthFromSesionStorgae } = authSlice.actions
+export default authSlice.reducer
