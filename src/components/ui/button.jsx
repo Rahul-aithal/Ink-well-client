a6f1ea9ff1646cbbcc65ce9110b535e0cf2528ba
Rahul-aithal <aithalrahul34@gmail.com>
2024-08-04 20:20:22 +0530

    Button changes in ui

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Button({ children, className,to="#" }) {
   return (
     <Link to={to}
-      className={`bg-gray-900 text-white dark:bg-white dark:text-black p-1 px-2 me-3 text-center font-inter cursor-pointer  hover:scale-100
+      className={`bg-gray-900 text-white dark:bg-white dark:text-black my-2 py-3 block text-center font-inter cursor-pointer  hover:scale-100
         ${className}`}
     >
       {children}
