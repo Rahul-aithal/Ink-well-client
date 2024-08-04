7e46feab257d62ba8b2cc6d2deb2e4b6aeb5bfeb
Rahul-aithal <aithalrahul34@gmail.com>
2024-08-04 20:20:55 +0530

    infinite moving cards component

---
+++ src/components/ui/infinite-moving-cards.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useRef, useState } from "react";
+import { cn } from "../../lib/utils";
+
+export const InfiniteMovingCards = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}) => {
+  const containerRef = useRef(null);
+  const scrollerRef = useRef(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+
+  const [start, setStart] = useState(false);
+
+  const addAnimation = () => {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  };
+
+  const getDirection = () => {
+    if (containerRef.current) {
+      containerRef.current.style.setProperty(
+        "--animation-direction",
+        direction === "left" ? "forwards" : "reverse"
+      );
+    }
+  };
+
+  const getSpeed = () => {
+    if (containerRef.current) {
+      let duration;
+      switch (speed) {
+        case "fast":
+          duration = "20s";
+          break;
+        case "normal":
+          duration = "40s";
+          break;
+        case "slow":
+          duration = "80s";
+          break;
+        default:
+          duration = "40s";
+      }
+      containerRef.current.style.setProperty("--animation-duration", duration);
+    }
+  };
+
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20 max-w-xl md:max-w-5xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex max-w-[90dvh] gap-4 py-4 flex-nowrap",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {items.map((item, idx) => (
+          <li
+          className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 px-8 py-6 md:w-[450px] 
+          bg-zinc-200 border-gray-300 text-black font-normal
+          dark:bg-gray-800 dark:border-gray-700 dark:text-gray-100 
+          shadow-lg"
+
+            key={item.name}
+          >
+            <blockquote>
+              <div
+                aria-hidden="true"
+                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+              ></div>
+              <span className="relative z-20 text-sm leading-[1.6]  ">
+                {item.quote}
+              </span>
+              <div className="relative z-20 mt-6 flex flex-row items-center">
+                <span className="flex flex-col gap-1">
+                  <span className="text-sm leading-[1.6] dark:text-gray-400 text-gray-600 ">
+                    {item.name}
+                  </span>
+                  <span className="text-sm leading-[1.6] dark:text-gray-400 text-gray-600 ">
+                    {item.title}
+                  </span>
+                </span>
+              </div>
+            </blockquote>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
